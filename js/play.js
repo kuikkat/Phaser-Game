51b8a6eacdcd960fa424e4b7621ec0bf5649e51d
Trillll <tony.kuikka@gmail.com>
2021-09-24 13:51:12 +0300

    Coin

--- js/play.js
+++ js/play.js
@@ -9,12 +9,18 @@ var playState = {
 		game.physics.arcade.enable(this.player);
 		this.player.body.gravity.y = 250;
 
+		this.coin = game.add.sprite(460, 300, 'coin');
+		game.physics.arcade.enable(this.coin);
+		this.coin.anchor.setTo(0.5, 0.5);
+
 		this.createWorld();
-	},
+
+		},
 
 	update: function() {
 
 		game.physics.arcade.collide(this.player, this.walls);
+		game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
 
 		//Move Player
 		this.movePlayer();
@@ -23,6 +29,31 @@ var playState = {
 
 			this.playerDie();
 		}
+	},
+
+	takeCoin: function(player, coin) {
+
+		this.updateCoinPosition();
+	},
+
+	updateCoinPosition: function() {
+
+		//Array
+		var coinPosition = [
+			{x: 140, y: 60}, {x: 360, y: 60}
+
+		];
+
+		for (var i = 0; i < this.updateCoinPosition.lenght; i++) {
+			if(coinPosition[i].x === this.coin.x) {
+				
+				coinPosition.splice(i, 1);
+			}
+		}
+
+		var newPosition = coinPosition[game.rnd.integerInRange(0, coinPosition.lenght -1)];
+		this.coin.reset(newPosition.x, newPosition.y);
+
 	},
 
 	movePlayer: function() {
@@ -56,11 +87,11 @@ var playState = {
 		game.add.sprite(480, 0, 'WallVertical', 0, this.walls);
 		game.add.sprite(0, 0, 'WallHorizontal', 0, this.walls);
 		game.add.sprite(300, 0, 'WallHorizontal', 0, this.walls);
-		game.add.sprite(0, 320, 'WallHorizontal', 0, this.walls);
-		game.add.sprite(300, 320, 'WallHorizontal', 0, this.walls);
+		game.add.sprite(0, 330, 'WallHorizontal', 0, this.walls);
+		game.add.sprite(320, 330, 'WallHorizontal', 0, this.walls);
 		game.add.sprite(-100, 160, 'WallHorizontal', 0, this.walls);
 		game.add.sprite(400, 160, 'WallHorizontal', 0, this.walls);
-		game.add.sprite(150, 230, 'WallHorizontal', 0, this.walls);
+		game.add.sprite(150, 240, 'WallHorizontal', 0, this.walls);
 		game.add.sprite(150, 90, 'WallHorizontal', 0, this.walls);
 		
 		this.walls.setAll('body.immovable', true);
