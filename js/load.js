340c09f8cb18a74c12f47fc5655570da790a5457
Trillll <tony.kuikka@gmail.com>
2021-09-24 09:58:02 +0300

    Load all assets

--- js/load.js
+++ js/load.js
@@ -5,9 +5,14 @@ var loadState = {
 		var loadingLabel = game.add.text(game.world.centerX, 150, 'loading...', { font: '30px Arial', fill: '#ffffff' });
 		loadingLabel.anchor.setTo(0.5, 0.5);
 
-		game.load.image('background', 'assets/images/background.png');
-
 		// Load all assets
+		game.load.image('background', 'assets/images/background.png');
+		game.load.image('coin', 'assets/images/coin.png');
+		game.load.image('player', 'assets/images/player.png');
+		game.load.image('enemy', 'assets/images/enemy.png');
+		game.load.image('pixel', 'assets/images/pixel.png');
+		game.load.image('WallHorizontal', 'assets/images/WallHorizontal.png');
+		game.load.image('WallVertical', 'assets/images/WallVertical.png');
 		
 		// ...
 	},
